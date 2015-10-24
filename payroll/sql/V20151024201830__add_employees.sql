d69bd85353bc8d1dbcfe225b718d5c69fae9f85a
corbett <cworkscode@gmail.com>
2015-10-24 15:54:11 -0500

    adding employee position view and added employees

---
+++ payroll/sql/V20151024201830__add_employees.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees(first_name, last_name, title_id) VALUES('Corbett', 'Martin', 1);
+INSERT INTO employees(first_name, last_name, title_id) VALUES('Doctor', 'Evil', 2);
+INSERT INTO employees(first_name, last_name, title_id) VALUES('Sponge', 'Bob', 3); 
