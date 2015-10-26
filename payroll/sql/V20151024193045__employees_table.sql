7aa71bad6c24bbeb55c857826b6baf1b1a6c0fb8
corbett <cworkscode@gmail.com>
2015-10-25 22:50:02 -0500

    created new file and copy database scripts, added birthdate and salary migrations

--- payroll/sql/V20151024193045__employees_table.sql
+++ payroll/sql/V20151024193045__employees_table.sql
@@ -1,6 +1,8 @@
+-- The first table
 CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
+  badge_id INT NULL,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB;
